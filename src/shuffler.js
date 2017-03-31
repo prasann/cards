2e8b520872c9f1f0d1d8a728823520196ae821d0
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2017-03-31 21:25:49 +0200

    Deploy latest

--- src/shuffler.js
+++
@@ -1,9 +0,0 @@
-import cardData from './data.json';
-
-const randomNumber = (max, min) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-export default function getCard() {
-  return cardData[randomNumber(5, 0)];
-}
