590161c07e72d659ca09a4ed398f97c9d296abd4
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2017-03-31 20:12:45 +0200

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Card extends Component {
+  render() {
+    <h1>Card</h1>
+  }
+}
+
+class App extends Component {
+  render() {
+    <Card></Card>
+  }
+}
+
+export default App;
