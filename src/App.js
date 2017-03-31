e29000a15efe6e3eec9f2640f908c783be8ef450
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2017-03-31 20:30:55 +0200

    Pass card data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class Card extends Component {
   render() {
+    const data = this.props.data;
     return <div>
-      <h1>Table</h1>
-      <h2>Tisch</h2>
-      <p> der Tisch </p>
-      <p>Tische</p>
+      <h1>{data.english}</h1>
+      <h2>{data.deutsch}</h2>
+      <p>{data.gender}</p>
+      <p>{data.plural}</p>
     </div>;
   }
 }
@@ -15,7 +16,7 @@ class Card extends Component {
 class App extends Component {
   render() {
     return <div className="container">
-      <Card></Card>
+      <Card data={{ english: 'Table', deutsch: 'Tisch', gender: 'der tisch', plural: 'tische' }}/>
     </div>
   }
 }
