d3452cfc7fda8e3484e97bcafe116b6854efcf9e
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2017-05-20 19:38:52 +0200

    Added slider to card

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ const presetData = {
   'N': { color: 'green', artikal: 'das' },
 };
 
+const SLIDER_TIME_IN_MS = 500;
+
 const genderForm = ({ g }) => (presetData[g].artikal);
 const genderColor = ({ g }) => (presetData[g].color);
 
 class FrontContent extends Component {
   render() {
     const data = this.props.data;
-    return <div className={`front ${this.props.fading === true ? 'fadeIn' :''}`}><h1>{data.en}</h1></div>
+    return <div className={`front`} ><h1>{data.en}</h1></div>
   }
 }
 
 class BackContent extends Component {
   render() {
     const data = this.props.data;
-    return <div className="back">
+    return <div className="back" >
       <h2>{`${genderForm(this.props.data)} ${data.de}`}</h2>
       <p><i>pl: </i>{`${data.pl}`}</p>
     </div>
@@ -31,18 +33,18 @@ class BackContent extends Component {
 class Card extends Component {
   constructor(props) {
     super();
-    this.state = { flipped: props.flipped, showData: props.data, fading: true };
+    this.state = { flipped: props.flipped, showData: props.data, inTransit: false };
     this.toggleFlip = this.toggleFlip.bind(this);
   }
 
   componentWillReceiveProps(props) {
+    this.setState({ flipped: false, inTransit: true });
     setTimeout(() => {
-      this.setState({ showData: props.data, fading: true })
-    }, 200);
-    this.setState({ flipped: false });
+      this.setState({ showData: props.data });
+    }, SLIDER_TIME_IN_MS / 2);
     setTimeout(() => {
-      this.setState({ fading: false })
-    }, 100);
+      this.setState({ inTransit: false });
+    }, SLIDER_TIME_IN_MS);
   }
 
   toggleFlip() {
@@ -52,10 +54,10 @@ class Card extends Component {
   render() {
     return <div onClick={this.toggleFlip}
                 style={{ color: genderColor(this.state.showData) }}
-                className="offset-lg-5 col-lg-2 offset-sm-2 col-sm-8">
-      <div className={`card flipper ${(this.state.flipped ? 'flip' : '')}`}>
-        <FrontContent data={this.state.showData} fading={this.state.fading}/>
-        <BackContent data={this.state.showData}/>
+                className={`${this.state.inTransit === true ? 'animate' : ''}`} >
+      <div className={`card flipper ${(this.state.flipped ? 'flip' : '')}`} >
+        <FrontContent data={this.state.showData} />
+        <BackContent data={this.state.showData} />
       </div>
     </div>;
   }
@@ -73,13 +75,15 @@ class App extends Component {
   }
 
   render() {
-    return <div className="container">
-      <div className="text-center offset-lg-2 col-lg-8 offset-sm-2 col-sm-8">
+    return <div className="container" >
+      <div className="text-center offset-lg-2 col-lg-8 offset-sm-2 col-sm-8" >
         <h1>Vokabeln Karten</h1>
       </div>
-      <Card data={this.state.card}/>
-      <div className="offset-lg-4 col-lg-4 offset-sm-2 col-sm-8">
-        <button className="next offset-lg-4 col-lg-4 offset-sm-1 col-sm-10" onClick={this.next}>Next</button>
+      <div className="card-container col-lg-2 col-sm-8 offset-lg-5 offset-sm-2" >
+        <Card data={this.state.card} />
+      </div>
+      <div className="offset-lg-4 col-lg-4 offset-sm-2 col-sm-8" >
+        <button className="next offset-lg-4 col-lg-4 offset-sm-1 col-sm-10" onClick={this.next} >Next</button>
       </div>
     </div>
   }
