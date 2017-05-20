4acd7517cda39f49165e9897de32131e01fcff54
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2017-05-20 17:06:11 +0200

    Add in slider

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ const genderColor = ({ g }) => (presetData[g].color);
 class FrontContent extends Component {
   render() {
     const data = this.props.data;
-    return <div className="front"><h1 style={{ color: genderColor(this.props.data) }}>{data.en}</h1></div>
+    return <div className={`front ${this.props.fading === true ? 'fadeIn' :''}`}><h1>{data.en}</h1></div>
   }
 }
 
 class BackContent extends Component {
   render() {
     const data = this.props.data;
-    return <div style={{ color: genderColor(this.props.data) }} className="back">
+    return <div className="back">
       <h2>{`${genderForm(this.props.data)} ${data.de}`}</h2>
       <p><i>pl: </i>{`${data.pl}`}</p>
     </div>
@@ -31,15 +31,18 @@ class BackContent extends Component {
 class Card extends Component {
   constructor(props) {
     super();
-    this.state = { flipped: props.flipped, showData: props.data };
+    this.state = { flipped: props.flipped, showData: props.data, fading: true };
     this.toggleFlip = this.toggleFlip.bind(this);
   }
 
   componentWillReceiveProps(props) {
+    setTimeout(() => {
+      this.setState({ showData: props.data, fading: true })
+    }, 200);
     this.setState({ flipped: false });
     setTimeout(() => {
-      this.setState({ showData: props.data })
-    }, 200);
+      this.setState({ fading: false })
+    }, 100);
   }
 
   toggleFlip() {
@@ -48,9 +51,10 @@ class Card extends Component {
 
   render() {
     return <div onClick={this.toggleFlip}
+                style={{ color: genderColor(this.state.showData) }}
                 className="offset-lg-5 col-lg-2 offset-sm-2 col-sm-8">
       <div className={`card flipper ${(this.state.flipped ? 'flip' : '')}`}>
-        <FrontContent data={this.state.showData}/>
+        <FrontContent data={this.state.showData} fading={this.state.fading}/>
         <BackContent data={this.state.showData}/>
       </div>
     </div>;
